a920c094b53f61eaf03f062c41152f927980dc52
Konstantin Ryzhankov <teffal@mail.ru>
2017-02-26 20:27:51 +0300

    no message

--- Home_task.py
+++ Home_task.py
@@ -1,56 +1,29 @@
-
-
-
-recipes_book = {
-    'яичница': [
-        {'ingredient_name': 'яйца', 'quantity': 2, 'measure': 'шт.'},
-        {'ingredient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'}
-    ],
-    'стейк': [
-        {'ingredient_name': 'говядина', 'quantity': 300, 'measure': 'гр.'},
-        {'ingredient_name': 'специи', 'quantity': 5, 'measure': 'гр.'},
-        {'ingredient_name': 'масло', 'quantity': 10, 'measure': 'мл.'}
-    ],
-    'салат': [
-        {'ingredient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'},
-        {'ingredient_name': 'огурцы', 'quantity': 100, 'measure': 'гр.'},
-        {'ingredient_name': 'масло', 'quantity': 100, 'measure': 'мл.'},
-        {'ingredient_name': 'лук', 'quantity': 1, 'measure': 'шт.'}
-    ]
-}
-
-with open('Book_cook.txt', 'w', encoding='UTF8') as f:
-    for recipe, ingredients in recipes_book.items():
-        f.write('\n' + str(recipe) + '\n' + str(len(ingredients)) + '\n')
-        for ingredient in ingredients:
-            contain_list = []
-            for contain in ingredient.values():
-                contain_list.append(contain)
-            f.write('|'.join(map(str, contain_list)) + '\n')
-
-with open('Book_cook.txt', 'r', encoding='UTF8') as f:
-    line = f.readlines()
-    cook_book = dict()
-    for i in range(len(line)):
-        # print(line[i])
-        if line[i] == '\n':
+# Ver. 2017.02.26
+def file_cook_book(my_file_path):
+    with open(my_file_path, 'r', encoding='UTF8') as f:
+        line = f.readline()
+        cook_book = dict()
+        while line:
+            while len(line.strip()) == 0:
+                line = f.readline()
+            my_kye = line.strip()
             list_ingredients = []
-            cook_book.update({line[i+1][:-1]:[]})
-            for n in range(int(line[i+2][:-1])):
-                value_dict = line[i+3+n][:-1].split('|')
+            line = f.readline()
+            for i in range(int(line.strip())):
+                value_dict = f.readline().strip().split('|')
                 ingredient_dict = {
                     'ingredient_name': value_dict[0],
                     'quantity': int(value_dict[1]),
                     'measure': value_dict[2]}
                 list_ingredients.append(ingredient_dict)
-            cook_book[line[i+1][:-1]] = list_ingredients
-# print(cook_book)
+            cook_book.update({my_kye: list_ingredients})
+            line = f.readline()
+    return cook_book
 
-def get_shop_list_by_dishes(dishes, person_count):
+def get_shop_list_by_dishes(dishes, person_count, my_file_path):
     shop_list = {}
     for dish in dishes:
-        for ingridient in cook_book[dish]:
-            # print(ingridient)
+        for ingridient in file_cook_book(my_file_path)[dish]:
             new_shop_list_item = dict(ingridient)
             new_shop_list_item['quantity'] *= person_count
             if new_shop_list_item['ingredient_name'] not in shop_list:
@@ -58,24 +31,18 @@ def get_shop_list_by_dishes(dishes, person_count):
             else:
                 shop_list[new_shop_list_item['ingredient_name']]['quantity'] += new_shop_list_item['quantity']
     return shop_list
-# print(shop_list)
+
 def print_shop_list(shop_list):
     for shop_list_item in shop_list.values():
-        print('{} {} {}'.format(shop_list_item['ingredient_name'],
-                                shop_list_item['quantity'],
-                                shop_list_item['measure']))
-    for shop_list_item in shop_list.values():
-        print('{1} {2} {0}'.format(shop_list_item['ingredient_name'],
-                                shop_list_item['quantity'],
-                                shop_list_item['measure']))
-    for shop_list_item in shop_list.values():
-        print('{quantity} {measure} {ingredient_name}'.format(**shop_list_item))
+        print('{ingredient_name}: {quantity} {measure} '.format(**shop_list_item))
 
 def create_shop_list():
+    my_file_path = input('Enter the path to your file (default path: Book_cook.txt):')
+    if len(my_file_path) == 0:
+        my_file_path = 'Book_cook.txt'
     person_count = int(input('Enter count of person: '))
-    # dishes = ['яичница', 'стейк', 'салат']
     dishes = input('Enter dishes for one person (you can list separated by "," without space): ').lower().split(',')
-    shop_list = get_shop_list_by_dishes(dishes, person_count)
+    shop_list = get_shop_list_by_dishes(dishes, person_count, my_file_path)
     print_shop_list(shop_list)
 
 create_shop_list()
